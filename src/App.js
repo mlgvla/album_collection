abc3cd992bdff4a5f5fb8362133408e12c0d1c86
Monica Gerard <mlgvla@gmail.com>
2023-05-20 12:21:26 -0400

    start here

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react"
 import "./App.css"
-import { albumCollection } from "./data"
-
-import AlbumList from "./components/AlbumList"
 
 function App() {
    return (
       <div className="App">
          <h1>My Music Album Collection</h1>
-         <AlbumList albums={albumCollection} />
+         <div className="albums">
+            <Album />
+         </div>
+      </div>
+   )
+}
+
+function Album() {
+   return (
+      <div className="album">
+         <h2>{"Title"}</h2>
+         {/* <p>{"Artist"}</p>
+         <p>{"Year"}</p>
+         <p>Rating: {"Rating"}</p>
+         <img src={"CoverUrl"} alt="" /> */}
       </div>
    )
 }
